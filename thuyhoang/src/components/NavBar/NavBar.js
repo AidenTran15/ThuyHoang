d2b12e219648a1e8dbcbfe2e5437063a47b9ea8b
https://github.com/AidenTran15 <aidenkiettran@gmail.com>
2024-10-02 00:55:02 +1000

    Remove individual links from NavBar and display only the dropdown menu

--- thuyhoang/src/components/NavBar/NavBar.js
+++ thuyhoang/src/components/NavBar/NavBar.js
@@ -7,12 +7,6 @@ const NavBar = () => {
   const location = useLocation();
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  // Determine current routes
-  const isHomePage = location.pathname === '/home';
-  const isLoginPage = location.pathname === '/';
-  const isDoneOrdersPage = location.pathname === '/done-orders';
-  const isProfilePage = location.pathname === '/profile';
-
   // Toggle dropdown state
   const toggleDropdown = () => {
     setDropdownOpen((prevState) => !prevState);
@@ -25,38 +19,22 @@ const NavBar = () => {
           <h1>Thuỷ Hoàng</h1>
         </div>
         <ul className="navbar-links">
-          {/* Dropdown Menu */}
+          {/* Only show the dropdown menu and remove other links */}
           <li className="navbar-dropdown">
             <button className="dropdown-button" onClick={toggleDropdown}>
               Menu
+              {/* Down arrow */}
+              <span className={`arrow ${dropdownOpen ? 'arrow-up' : 'arrow-down'}`}></span>
             </button>
             {dropdownOpen && (
               <div className="dropdown-content">
-                {/* Show "Trang Chủ" link */}
+                {/* Dropdown links */}
                 <Link to="/home" onClick={() => setDropdownOpen(false)}>Trang Chủ</Link>
-
-                {/* Show "Đơn Hoàn Thành" link */}
                 <Link to="/done-orders" onClick={() => setDropdownOpen(false)}>Đơn Hoàn Thành</Link>
-
-                {/* Show "Hồ Sơ" link */}
                 <Link to="/profile" onClick={() => setDropdownOpen(false)}>Hồ Sơ</Link>
               </div>
             )}
           </li>
-
-          {/* On the "Đơn Hoàn Thành" page, show "Trang Chủ" link separately */}
-          {isDoneOrdersPage && (
-            <li>
-              <Link to="/home">Trang Chủ</Link>
-            </li>
-          )}
-
-          {/* On the "Trang Chủ" page, show "Đơn Hoàn Thành" link separately */}
-          {isHomePage && (
-            <li>
-              <Link to="/done-orders">Đơn Hoàn Thành</Link>
-            </li>
-          )}
         </ul>
       </nav>
     </div>
