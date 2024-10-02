173adf10de417985a9a054b69ff98a3a9c4bbb97
https://github.com/AidenTran15 <aidenkiettran@gmail.com>
2024-10-02 14:39:16 +1000

    I would like to commit, help me think the commit message pls

--- thuyhoang/src/App.js
+++ thuyhoang/src/App.js
@@ -1,6 +1,5 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage';
 import HomePage from './pages/HomePage/HomePage';
 import DoneOrders from './pages/DoneOrders/DoneOrders';
@@ -8,18 +7,34 @@ import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import ProfilePage from './pages/ProfilePage/ProfilePage'; // Import ProfilePage component
 
+// Component to manage layout with conditional NavBar and Footer
+const Layout = ({ children }) => {
+  const location = useLocation();
+  const noNavBarFooterRoutes = ['/login', '/']; // Routes where NavBar and Footer should not be displayed
+
+  return (
+    <>
+      {/* Conditionally render NavBar based on the current path */}
+      {!noNavBarFooterRoutes.includes(location.pathname) && <NavBar />}
+      {children}
+      {/* Conditionally render Footer based on the current path */}
+      {!noNavBarFooterRoutes.includes(location.pathname) && <Footer />}
+    </>
+  );
+};
+
 const App = () => {
   return (
     <Router>
-      <NavBar /> {/* NavBar will appear at the top of every page */}
-      <Routes>
-        <Route path="/done-orders" element={<DoneOrders />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile" element={<ProfilePage />} /> {/* Add ProfilePage Route */}
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-      <Footer /> {/* Footer will appear at the bottom of every page */}
+      <Layout>
+        <Routes>
+          <Route path="/done-orders" element={<DoneOrders />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile" element={<ProfilePage />} /> {/* Add ProfilePage Route */}
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
